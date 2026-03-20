e03b34b8c4195becee7a132af799f951eac2a692
Sergio Rua <sergio@axonops.com>
2026-03-20 10:00:20 +0000

    Adds tf for testing

---
+++ test/e2e/terraform/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    exoscale = {
+      source  = "exoscale/exoscale"
+      version = ">= 0.62.0"
+    }
+  }
+}
+
+variable "exoscale_api_key" {
+  description = "Exoscale API key. Can also be set via the EXOSCALE_API_KEY environment variable."
+  type        = string
+  default     = null
+  sensitive   = true
+}
+
+variable "exoscale_api_secret" {
+  description = "Exoscale API secret. Can also be set via the EXOSCALE_API_SECRET environment variable."
+  type        = string
+  default     = null
+  sensitive   = true
+}
+
+variable "exoscale_ssh_public_key" {
+  description = "SSH public key allowing access to these instances"
+  type        = string
+  sensitive   = true
+}
+
+provider "exoscale" {
+  key    = var.exoscale_api_key
+  secret = var.exoscale_api_secret
+}
+
+resource "exoscale_ssh_key" "ssh_key" {
+  name       = "github-ssh-key"
+  public_key = var.exoscale_ssh_public_key
+}
+
+module "k3s" {
+  source            = "github.com/digitalis-io/terraform-exoscale-k3s"
+  ssh_key_name      = "github-ssh-key"
+  agent_count       = 3
+  ssh_allowed_cidrs = ["0.0.0.0/0"]
+}
+
+output "server_ip" {
+  value = module.k3s.server_public_ip
+}
+
+output "kubeconfig_cmd" {
+  value = module.k3s.kubeconfig_command
+}
+
